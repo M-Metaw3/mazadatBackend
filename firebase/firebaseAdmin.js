1aa732bfe2d81270f69221737bda42e9b415d402
M-Metaw3 <mahmoudmetawea161@gmail.com>
2024-06-11 21:35:27 +0300

    check firebase

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,14 +1,14 @@
 // server.js or your main entry file
 const path = require('path');
 
-
+const fs = require('fs');
 // Initialize Firebase Admin SDK
 const admin = require('firebase-admin');
 
 const serviceAccountPath = path.resolve(__dirname, process.env.GOOGLE_APPLICATION_CREDENTIALS);
 console.log(serviceAccountPath)
-if (!serviceAccountPath) {
-  throw new Error('Service account path is not defined');
+if (!fs.existsSync(serviceAccountPath)) {
+  throw new Error(`Service account file not found at path: ${serviceAccountPath}`);
 }
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountPath),
