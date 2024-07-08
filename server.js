fdcaca029e5c2a3bd42d9c485415ef4f4513104c
M-Metaw3 <mahmoudmetawea161@gmail.com>
2024-07-08 13:47:50 +0300

    new changes

--- server.js
+++ server.js
@@ -127,15 +127,15 @@ const dbName = process.env.NODE_ENV_docker === 'production' ? 'mazadat-prod' : '
 //     console.error('DB connection error:', err);
 //   });
 
-// const port = process.env.PORT || 3000;
-// const server = app.listen(port, () => {
-//   console.log(`App running on port ${port}...`);
-// });
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
 
-// process.on('unhandledRejection', err => {
-//   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-//   console.log(err.name, err.message);
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
