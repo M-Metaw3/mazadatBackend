ea8699b4e9d213d73dea160b9ff3eedc882cee59
M-Metaw3 <mahmoudmetawea161@gmail.com>
2024-07-14 13:05:12 +0300

    new changes

--- controllers/authcontroller/authcontroller.js
+++ controllers/authcontroller/authcontroller.js
@@ -12,9 +12,7 @@ const catchAsync = require('../../utils/catchAsync');
 
 
 const signToken = id => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
-  })
+  return jwt.sign({ id }, process.env.JWT_SECRET)
 };
 
 
