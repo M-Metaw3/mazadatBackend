4ffc46b15776b8472fa1ca04c0fd648fbeb4f9ea
M-Metaw3 <mahmoudmetawea161@gmail.com>
2024-07-14 11:55:31 +0300

    expired for jwt

--- controllers/authcontroller/authcontroller.js
+++ controllers/authcontroller/authcontroller.js
@@ -12,7 +12,9 @@ const catchAsync = require('../../utils/catchAsync');
 
 
 const signToken = id => {
-  return jwt.sign({ id }, process.env.JWT_SECRET )
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  })
 };
 
 
