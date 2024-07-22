dc2645ba167218e82f2fc6ce0a08f9a760b85247
M-Metaw3 <mahmoudmetawea161@gmail.com>
2024-07-22 14:52:56 +0300

    aaaaaaaaaaaaaaaaaaaaaaaa

--- controllers/authcontroller/authcontroller.js
+++ controllers/authcontroller/authcontroller.js
@@ -854,14 +854,15 @@ const blockUser = async (req, res) => {
 const logoutUser = async (req, res) => {
   try {
     const userId = req.user.id;
+    console.log(userId)
 
     // Update isLogin status to false
-    await User.findByIdAndUpdate(userId, {
+  const a=  await User.findByIdAndUpdate(userId, {
       isLogin: false,
       authToken: null,
       deviceDetails: {}
     });
-
+console.log(a)
     res.status(200).json({ message: 'Logged out successfully.' });
   } catch (error) {
     res.status(500).json({ message: 'An error occurred during logout.', error: error.message });
